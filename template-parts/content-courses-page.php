4e3548a2d6d5ace5bc887e2e0d6acf2c9c8ba1e1
Mario Badilla <mario.badilla@gmail.com>
2018-10-29 15:17:54 -0600

    archive cursos v1

---
+++ template-parts/content-courses-page.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package pemscores
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+	if ( has_post_thumbnail() ) { ?>
+	<figure class="featured-image full-bleed">
+		<?php
+		the_post_thumbnail('pemscores-full-bleed');
+		?>
+	</figure><!-- .featured-image full-bleed -->
+	<?php } ?>
+
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+
+	<div class="entry-content post-content">
+		<?php
+			the_content();
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Páginas:', 'pemscores' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content .post-content -->
+
+	<?php
+	// If comments are open or we have at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) :
+		comments_template();
+	endif;
+	?>
+</article><!-- #post-## -->

9c3eec4a58b07f0865708c7ed7df67d6e33895de
marbaque <mario.badilla@gmail.com>
2018-10-30 11:48:45 -0600

    archive cursos v2

--- template-parts/content-courses-page.php
+++ template-parts/content-courses-page.php
@@ -8,6 +8,9 @@
  */
 
 ?>
+<header class="entry-header">
+	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+</header><!-- .entry-header -->
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
@@ -19,9 +22,7 @@
 	</figure><!-- .featured-image full-bleed -->
 	<?php } ?>
 
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+
 
 
 	<div class="entry-content post-content">
@@ -35,10 +36,4 @@
 		?>
 	</div><!-- .entry-content .post-content -->
 
-	<?php
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) :
-		comments_template();
-	endif;
-	?>
 </article><!-- #post-## -->

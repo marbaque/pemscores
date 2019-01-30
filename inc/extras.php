5b9072467a7be0143072cd292e7acc21705ec902
Mario Badilla <mario.badilla@gmail.com>
2019-01-30 15:27:38 -0600

    single recurso get_the_term_list

--- inc/extras.php
+++ inc/extras.php
@@ -125,3 +125,21 @@ add_shortcode( 'snapshot', function ( $atts ) {
 
 	return '<img src="' . esc_attr( $src ) . '" alt="' . esc_attr( $atts['alt'] ) . '"/>';
 } );
+
+
+//Usar gutenberg en recursos
+add_filter('register_post_type_args', 'learnpress_cpt_add_gutenberg_support', 10, 2);
+
+function learnpress_cpt_add_gutenberg_support ($args, $post_type)
+{
+
+    if (in_array($post_type, array(
+       //'lp_course',
+       'lp_lesson',
+           ))){
+
+        $args['show_in_rest'] = true;
+    }
+
+    return $args;
+}

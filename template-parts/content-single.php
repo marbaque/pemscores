373a698cd34f493187fad27cc80303f8d7188e4b
Mario Badilla <mario.badilla@gmail.com>
2019-02-25 16:29:54 -0600

    quitar imagen duplicada de single

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -10,14 +10,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php
-	if ( has_post_thumbnail() ) { ?>
-	<figure class="featured-image full-bleed">
-		<?php
-		the_post_thumbnail('pemscores-full-bleed');
-		?>
-	</figure><!-- .featured-image full-bleed -->
-	<?php } ?>
 
 	<header class="entry-header">
 		<?php pemscores_the_category_list(); ?>

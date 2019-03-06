1e7886190c1314820cd76ff611eadb89e44bd4a9
marbaque <mario.badilla@gmail.com>
2019-03-05 20:53:36 -0600

    prueba con colores nuevos

--- header.php
+++ header.php
@@ -25,32 +25,34 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<?php if ( has_custom_logo() ) : ?>
-				<div class="site-logo"><?php the_custom_logo(); ?></div>
 
-			<?php else: ?>
-				<?php $blog_info = get_bloginfo( 'name' ); ?>
+			<?php $blog_info = get_bloginfo( 'name' ); ?>
 
-				<?php if ( ! empty( $blog_info ) ) : ?>
-					<?php if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<div class="site-branding__text">
+				<?php if ( has_custom_logo() ) : ?>
+					<div class="site-logo"><?php the_custom_logo(); ?></div>
+
+				<?php else: ?>	
+					<?php if ( ! empty( $blog_info ) ) : ?>
+						<?php if ( is_front_page() && is_home() ) : ?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php else : ?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php endif; ?>
 					<?php endif; ?>
+
+					<?php
+					$description = get_bloginfo( 'description', 'display' );
+					if ( $description || is_customize_preview() ) :
+						?>
+							<p class="site-description">
+								<?php echo $description; ?>
+							</p>
+					<?php endif; ?>
+
 				<?php endif; ?>
 
-				<?php
-				$description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) :
-					?>
-						<p class="site-description">
-							<?php echo $description; ?>
-						</p>
-				<?php endif; ?>
-			<?php endif; ?>
-
-
-
+			</div><!-- .site-branding__text -->
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">

0db122da2ba56587e54255c02e9b5d6f43862577
Mario Badilla <mario.badilla@gmail.com>
2019-05-06 13:43:35 -0600

    mostrar barra a editores y autores

--- inc/extras.php
+++ inc/extras.php
@@ -280,10 +280,12 @@ add_filter( 'enter_title_here', 'pemscores_change_title_text' );
 add_action( 'init', 'remove_admin_bar_user', 10001 );
 function remove_admin_bar_user() {
 
-	if ( current_user_can( 'administrator' ) || is_admin() ) {
-
+	if ( current_user_can( 'administrator' ) || current_user_can( 'editor' ) || current_user_can( 'author' ) ) {
+		
 		show_admin_bar( true );
+	
 	} else {
+		
 		show_admin_bar( false );
 	}
 }

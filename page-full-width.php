e3fb50b6a7c3fe2e237e9f4cc7ff43bab7eeb8ed
marbaque <mario.badilla@gmail.com>
2019-07-16 09:50:27 -0600

    template gull width

---
+++ page-full-width.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Template Name: Full Width
+*
+* @package WordPress
+* @subpackage Twenty_Fourteen
+* @since Twenty Fourteen 1.0
+*/
+
+get_header(); ?>
+	<div id="primary" class="content-area full-width">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();

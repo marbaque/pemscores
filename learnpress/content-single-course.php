53846c3054247b7d450f8190e1e9e502ef0b5b96
marbaque <mariobadilla@gmail.com>
2020-05-19 17:00:36 -0600

    tamaño de imagen de portada, campos de referencias y creditos

--- learnpress/content-single-course.php
+++ learnpress/content-single-course.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template for displaying course content within the loop.
  *
@@ -12,9 +13,9 @@
 /**
  * Prevent loading this file directly
  */
-defined( 'ABSPATH' ) || exit();
+defined('ABSPATH') || exit();
 
-if ( post_password_required() ) {
+if (post_password_required()) {
 	echo get_the_password_form();
 
 	return;
@@ -23,23 +24,23 @@ if ( post_password_required() ) {
 /**
  * @deprecated
  */
-do_action( 'learn_press_before_main_content' );
-do_action( 'learn_press_before_single_course' );
-do_action( 'learn_press_before_single_course_summary' );
+do_action('learn_press_before_main_content');
+do_action('learn_press_before_single_course');
+do_action('learn_press_before_single_course_summary');
 
 /**
  * @since 3.0.0
  */
-do_action( 'learn-press/before-main-content' );
+do_action('learn-press/before-main-content');
 
-do_action( 'learn-press/before-single-course' );
+do_action('learn-press/before-single-course');
 
 ?>
 
-<?php if ( get_field('anuncio_curso') ): ?>
-        <div class="anuncio"><?php the_field('anuncio_curso'); ?></div>
-		<?php endif; ?>
-		
+<?php if (get_field('anuncio_curso')) : ?>
+	<div class="anuncio"><?php the_field('anuncio_curso'); ?></div>
+<?php endif; ?>
+
 <div id="learn-press-course" class="course-summary">
 	<?php
 	/**
@@ -47,7 +48,7 @@ do_action( 'learn-press/before-single-course' );
 	 *
 	 * @see learn_press_single_course_summary()
 	 */
-	do_action( 'learn-press/single-course-summary' );
+	do_action('learn-press/single-course-summary');
 	?>
 </div>
 <?php
@@ -55,13 +56,13 @@ do_action( 'learn-press/before-single-course' );
 /**
  * @since 3.0.0
  */
-do_action( 'learn-press/after-main-content' );
+do_action('learn-press/after-main-content');
 
-do_action( 'learn-press/after-single-course' );
+do_action('learn-press/after-single-course');
 
 /**
  * @deprecated
  */
-do_action( 'learn_press_after_single_course_summary' );
-do_action( 'learn_press_after_single_course' );
-do_action( 'learn_press_after_main_content' );
+do_action('learn_press_after_single_course_summary');
+do_action('learn_press_after_single_course');
+do_action('learn_press_after_main_content');
--- learnpress/single-course/tabs/tabs.php
+++ learnpress/single-course/tabs/tabs.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template for displaying tab nav of single course.
  *
@@ -12,40 +13,93 @@
 /**
  * Prevent loading this file directly
  */
-defined( 'ABSPATH' ) || exit();
+defined('ABSPATH') || exit();
 ?>
 
 <?php $tabs = learn_press_get_course_tabs(); ?>
 
-<?php if ( empty( $tabs ) ) {
+<?php if (empty($tabs)) {
 	return;
 } ?>
 
 <div id="learn-press-course-tabs" class="course-tabs">
 
-    <!-- <ul class="learn-press-nav-tabs course-nav-tabs"> -->
+	<ul class="learn-press-nav-tabs course-nav-tabs">
 
-    <!-- </ul> -->
+		<?php foreach ($tabs as $key => $tab) { ?>
 
-	<?php foreach ( $tabs as $key => $tab ) { ?>
+			<?php $classes = array('course-nav course-nav-tab-' . esc_attr($key));
+			if (!empty($tab['active']) && $tab['active']) {
+				$classes[] = 'active default';
+			} ?>
 
-        <div class="course-overview_section">
-			<h3><?php echo $tab['title']; ?></h3>
+			<li class="<?php echo join(' ', $classes); ?>">
+				<a href="?tab=<?php echo esc_attr($tab['id']); ?>" data-tab="#<?php echo esc_attr($tab['id']); ?>"><?php echo $tab['title']; ?></a>
+			</li>
+
+		<?php } ?>
+
+		<?php if (get_field('lp_referencias')) : ?>
+
+			<!-- Custom tab Referencias -->
+			<li class="<?php echo join(' ', $classes); ?>">
+				<a href="?tab=tab-referencias" data-tab="#lp_ref">Referencias</a>
+			</li>
+
+		<?php endif; ?>
+
+		<?php if (get_field('lp_creditos')) : ?>
+
+			<!-- Custom tab Creditos -->
+			<li class="<?php echo join(' ', $classes); ?>">
+				<a href="?tab=tab-creditos" data-tab="#lp_cred">Créditos</a>
+			</li>
+
+		<?php endif; ?>
+
+	</ul>
+
+	<?php foreach ($tabs as $key => $tab) { ?>
+
+		<div class="course-tab-panel-<?php echo esc_attr($key); ?> course-tab-panel<?php echo !empty($tab['active']) && $tab['active'] ? ' active' : ''; ?>" id="<?php echo esc_attr($tab['id']); ?>">
+
 			<?php
-			if ( apply_filters( 'learn_press_allow_display_tab_section', true, $key, $tab ) ) {
-				if ( is_callable( $tab['callback'] ) ) {
-					call_user_func( $tab['callback'], $key, $tab );
+			if (apply_filters('learn_press_allow_display_tab_section', true, $key, $tab)) {
+				if (is_callable($tab['callback'])) {
+					call_user_func($tab['callback'], $key, $tab);
 				} else {
 					/**
 					 * @since 3.0.0
 					 */
-					do_action( 'learn-press/course-tab-content', $key, $tab );
+					do_action('learn-press/course-tab-content', $key, $tab);
 				}
 			}
 			?>
 
-        </div>
+		</div>
 
 	<?php } ?>
+
+	<?php if (get_field('lp_referencias')) : ?>
+
+		<!-- Custom tab panel Referencias -->
+		<div class="course-tab-panel-referencias course-tab-panel<?php echo !empty($tab['active']) && $tab['active'] ? ' active' : ''; ?>" id="lp_ref">
+
+			<?php the_field('lp_referencias'); ?>
+
+		</div>
+
+	<?php endif; ?>
+
+	<?php if (get_field('lp_creditos')) : ?>
+
+		<!-- Custom tab panel Creditos -->
+		<div class="course-tab-panel-creditos course-tab-panel<?php echo !empty($tab['active']) && $tab['active'] ? ' active' : ''; ?>" id="lp_cred">
+
+			<?php the_field('lp_creditos'); ?>
+
+		</div>
+
+	<?php endif; ?>
 
 </div>

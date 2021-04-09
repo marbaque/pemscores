262c9e8e72bb6e266ea36f99ac54552304520793
mbadilla <mario.badilla@gmail.com>
2021-04-09 11:41:42 -0600

    Incluir fontawesome 5 en las funciones del tema

--- functions.php
+++ functions.php
@@ -297,6 +297,9 @@ function pemscores_scripts()
 
 	wp_enqueue_script('pemscores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
 
+	wp_enqueue_style( 'font-awesome-free', '//use.fontawesome.com/releases/v5.15.3/css/all.css' );
+
+	
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
 	}
